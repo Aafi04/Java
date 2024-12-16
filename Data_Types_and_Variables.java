c00c116aeca4e5251c438bdbab822fee6dda21d1
Aafi04 <mdaafi04@gmail.com>
2024-12-16 23:38:22 +0530

    DTV Q1

---
+++ Data_Types_and_Variables.java
@@ -0,0 +1,24 @@
+// 1. Write a program to declare and initialize variables of all primitive data types and print
+// their values.
+
+public class Data_Types_and_Variables {
+    static int a = 10;
+    static float b = 10.5f;
+    static double c = 10.02020d;
+    static long d = 65432154;
+    static byte e = 12;
+    static char f = 'A';
+    static String g = "Aafi";
+    static boolean h = true;
+
+    public static void main(String[] args) {
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Double: " + c);
+        System.out.println("Long: " + d);
+        System.out.println("Byte: " + e);
+        System.out.println("Char: " + f);
+        System.out.println("String: " + g);
+        System.out.println("Boolean: " + h);
+    }
+}
