9ad751ce22f03b19c7f3264539a54536a0c9eb44
Aafi04 <mdaafi04@gmail.com>
2024-12-17 08:41:46 +0530

    DTV Q2

--- Data_Types_and_Variables.java
+++ Data_Types_and_Variables.java
@@ -1,24 +1,23 @@
 // 1. Write a program to declare and initialize variables of all primitive data types and print
 // their values.
 
-public class Data_Types_and_Variables {
-    static int a = 10;
-    static float b = 10.5f;
-    static double c = 10.02020d;
-    static long d = 65432154;
-    static byte e = 12;
-    static char f = 'A';
-    static String g = "Aafi";
-    static boolean h = true;
+/*
+ * public class Data_Types_and_Variables { static int a = 10; static float b = 10.5f; static double
+ * c = 10.02020d; static long d = 65432154; static byte e = 12; static char f = 'A'; static String g
+ * = "Aafi"; static boolean h = true;
+ * 
+ * public static void main(String[] args) { System.out.println("Integer: " + a);
+ * System.out.println("Float: " + b); System.out.println("Double: " + c);
+ * System.out.println("Long: " + d); System.out.println("Byte: " + e); System.out.println("Char: " +
+ * f); System.out.println("String: " + g); System.out.println("Boolean: " + h); } }
+ */
 
-    public static void main(String[] args) {
-        System.out.println("Integer: " + a);
-        System.out.println("Float: " + b);
-        System.out.println("Double: " + c);
-        System.out.println("Long: " + d);
-        System.out.println("Byte: " + e);
-        System.out.println("Char: " + f);
-        System.out.println("String: " + g);
-        System.out.println("Boolean: " + h);
-    }
-}
+// 2. Create a program to swap the values of two variables using a third variable.
+
+/*
+ * public class Data_Types_and_Variables { public static void main(String[] args) { int a = 10; int
+ * b = 20; System.out.println("Value of a before swap: " + a + " and value of b before swap: " + b);
+ * int c = a; a = b; b = c; System.out.println("Value of a after swap: " + a +
+ * " and value of b after swap: " + b); } }
+ */
+
